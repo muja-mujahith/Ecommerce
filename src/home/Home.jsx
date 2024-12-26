3b8f29964c40331a8b26222bac6bdf72665f6718
msm.mujahith <mujahith18862.new@gmail.com>
2024-12-26 13:18:00 +0530

    third commit

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import HomeCategory from './HomeCategory'
+import Categoryshowcase from './Categoryshowcase'
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <HomeCategory />
+      <Categoryshowcase />
     </div>
   )
 }
